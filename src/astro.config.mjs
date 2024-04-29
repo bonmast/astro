a41301fe134b837d6430040087e8a3e66caf01e6
Simon Fletcher <simon@simonserver.co.uk>
2024-04-29 22:19:54 +0100

    Update CSS

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -6,6 +6,9 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Astro with Starlight Lab',
+			customCss: [
+				'./src/styles/lab.css'
+			],
 			social: {
 				github: 'https://github.com/bonmast/astro-lab',
 			},
